1a82e9cbb20d7ab7c7d5f381615be50b25ffa9f8
Adrian <66483092+serbinskis@users.noreply.github.com>
2025-02-27 12:55:40 +0200

    Update and rename tree_visualizer.pu to tree_visualizer.py

--- tree_visualizer.py
+++ tree_visualizer.py
@@ -3,11 +3,12 @@ class TreeNode:
     self.text = text
     self.parent = parent
     self.children = []
-    self.width = -1;
-    self.height = -1;
-    self.poistion = [-1, -1]
-    self.depth = -1;
-    self.depth_count = -1;
+    self.sibling = None #Sigling is next node in the same depth
+    self.width = -1 #Calculate based on text width
+    self.height = -1 #Calculate based on text height
+    self.position = [-1, -1] #Calculate based on depth and depth_count + width and height
+    self.depth = -1 #Nodes depth, I guess we can start with 1 or 0, as you like
+    self.depth_count = -1 #How many nodes there are on the same depth
 
   def add_children(self, child_node):
     self.children.append(child_node)
@@ -17,7 +18,7 @@ class TreeVizualizer:
   def __init__(self, root):
     self.root = root
 
-  def start(self):
+  def start(self)
     #Begin new thread and render tree each frame
     #In new thread we create window and call fucntion which will
     #Each node position and then render it
